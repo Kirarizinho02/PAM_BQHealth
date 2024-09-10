34afeafc1c3beded25e14c505bf77a1d0c87279c
Kiras Augusto <kirarizinho@gmail.com>
2024-09-09 22:03:01 -0300

    Update general_appbar.dart

--- app_review/lib/src/components/general_appbar.dart
+++ app_review/lib/src/components/general_appbar.dart
@@ -1,11 +1,11 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';  //Importação do pacote de widgets e temas do flutter
 
-  var generateAppBar = AppBar(
-      title: const Text('BQ Health', style: styleTitle,),
-      centerTitle: true,
+  var generateAppBar = AppBar( //Gera a varíavel AppBar como um widget appBar
+      title: const Text('BQ Health', style: styleTitle,), //O título da appBar será BQ Health e seu estilo vem da constante styleTitle abaixo
+      centerTitle: true, //Configura a centralização do título como verdadeira
     );
 
-const styleTitle = TextStyle(
-  fontSize: 14,
-  color: Colors.blue,
+const styleTitle = TextStyle( //Constroi o estilo do texto usado no título da appBar
+  fontSize: 14, //Tamanho da fonte 14 pixels
+  color: Colors.blue, //Fonte de cor azul nativa do dart
   );
