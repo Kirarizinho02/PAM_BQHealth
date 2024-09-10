afdb0af5b68b2a98b09f16a5619940e586b2115a
Kiras Augusto <kirarizinho@gmail.com>
2024-09-09 21:57:13 -0300

    Update title_imc.dart

--- app_review/lib/src/components/title_imc.dart
+++ app_review/lib/src/components/title_imc.dart
@@ -1,10 +1,10 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; //Importação do pacote de widgets e temas do flutter
 
-class TitleIMC extends StatelessWidget {
-  const TitleIMC({super.key});
+class TitleIMC extends StatelessWidget { //Construção do widget TitleIMC como um stateless widget
+  const TitleIMC({super.key}); //Definição do widget como constante 
 
   @override
-  Widget build(BuildContext context) {
-    return const Text('BQ Health', style: TextStyle(fontSize: 20.0));
+  Widget build(BuildContext context) { //Chama o método build para a construção da interfáce gráfica
+    return const Text('BQ Health', style: TextStyle(fontSize: 20.0)); //Define o texto que será exibido e o seu tamanho, também marcando-o como const
   }
 }
