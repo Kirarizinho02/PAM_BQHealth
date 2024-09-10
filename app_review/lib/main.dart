3513110bed737899da7e01c923843eb24752c3e0
Kiras Augusto <kirarizinho@gmail.com>
2024-09-09 21:39:19 -0300

    comentários aqui

--- app_review/lib/main.dart
+++ app_review/lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
-import 'package:app_review/src/my_app.dart';
+import 'package:flutter/material.dart'; //Importação do pacote de widgets e temas do flutter
+import 'package:app_review/src/my_app.dart'; //Importação do Widget my_app.dart, estruturando o aplicativo
 
-void main() {
-  runApp(const MyApp());
+void main() { 
+  runApp(const MyApp()); //Inicializa o widget MyApp() como uma instância da classe MyApp
 }
